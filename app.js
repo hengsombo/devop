b412bd8e4c04cc96761182ce7eab964c0b273261
hengsombo <hengsombo@yahoo.com>
2024-12-04 15:46:46 +0700

    SOMBO

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World SOMBOHENG1');
+    res.status(200).send('Hello World SOMBOHENG2');
 });
 
 app.get('/user/:id', (req, res) => {
